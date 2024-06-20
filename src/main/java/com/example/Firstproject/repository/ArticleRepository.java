7dd7d8b93dca2384cebb027b166bd8986afbb067
sangmin1120 <sangmin8276@gmail.com>
2024-06-20 14:56:38 +0900

    DB CURDrepository 상속받은 인터페이스

--- src/main/java/com/example/Firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/Firstproject/repository/ArticleRepository.java
@@ -1,2 +1,8 @@
-package com.example.Firstproject.repository;public class ArticleRepository {
+package com.example.Firstproject.repository;
+
+import com.example.Firstproject.entity.Article;
+//curdrepository를 자동 임포트 해준다.
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 }
