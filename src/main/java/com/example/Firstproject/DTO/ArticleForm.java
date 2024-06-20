a684175da7731c3580fe8557526758a4c69b4fe2
sangmin1120 <sangmin8276@gmail.com>
2024-06-20 14:24:12 +0900

    form을 DTO로 변환

--- src/main/java/com/example/Firstproject/DTO/ArticleForm.java
+++ src/main/java/com/example/Firstproject/DTO/ArticleForm.java
@@ -1,2 +1,19 @@
-package com.example.Firstproject.DTO;public class ArticleForm {
+package com.example.Firstproject.DTO;
+
+public class ArticleForm {
+    private String title; //제목을 받을 필드
+    private String content; // 내용을 받을 필드
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
