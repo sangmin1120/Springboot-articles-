f095132a33ef1d48186a66aa88daa8d0a5119e31
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 00:30:29 +0900

    member entity

--- src/main/java/com/example/Firstproject/entity/Member.java
+++ src/main/java/com/example/Firstproject/entity/Member.java
@@ -1,2 +1,33 @@
-package com.example.Firstproject.entity;public class Member {
+package com.example.Firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+    public Member(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
