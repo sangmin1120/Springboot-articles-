e83317a7b29b91f5b070e9e7c70a7c38d3ca242b
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 00:29:52 +0900

    엔티티를 DB에 저장 추가

--- src/main/java/com/example/Firstproject/controller/MemberController.java
+++ src/main/java/com/example/Firstproject/controller/MemberController.java
@@ -1,2 +1,34 @@
-package com.example.Firstproject.controller;public class MemberController {
+package com.example.Firstproject.controller;
+
+import com.example.Firstproject.entity.Member;
+import com.example.Firstproject.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.Firstproject.DTO.MemberForm;
+
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping("/members/new")
+    public String newMemberForm(){
+
+        return "members/new";
+    }
+
+    @PostMapping("/members/create")
+    public String createMember(MemberForm form){
+        // System.out.println(form.toString());
+        // 1. DTO->엔티티로 변환
+        Member member = form.toEntity();
+        // System.out.println(member.toString());
+
+        // 2. repository에 저장
+        Member saved = memberRepository.save(member);
+        System.out.println(saved.toString());
+        return "";
+    }
 }
