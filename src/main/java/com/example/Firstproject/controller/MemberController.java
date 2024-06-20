fbd2372d0ba94340d73730f00a4338f1c0b89fae
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 03:38:28 +0900

    refactoring

--- src/main/java/com/example/Firstproject/controller/MemberController.java
+++ src/main/java/com/example/Firstproject/controller/MemberController.java
@@ -2,12 +2,14 @@ package com.example.Firstproject.controller;
 
 import com.example.Firstproject.entity.Member;
 import com.example.Firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.Firstproject.DTO.MemberForm;
 
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -28,7 +30,7 @@ public class MemberController {
 
         // 2. repository에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/Firstproject/entity/Member.java
+++ src/main/java/com/example/Firstproject/entity/Member.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Member {
     @Id
@@ -15,19 +19,4 @@ public class Member {
     private String email;
     @Column
     private String password;
-
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
