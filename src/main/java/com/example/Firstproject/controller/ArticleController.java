1ce4def9d9819a512a4c0b189b5db81ebc0b5f9b
sangmin1120 <sangmin8276@gmail.com>
2024-06-20 14:23:40 +0900

    form 데이터 controller

--- src/main/java/com/example/Firstproject/controller/ArticleController.java
+++ src/main/java/com/example/Firstproject/controller/ArticleController.java
@@ -1,2 +1,22 @@
-package com.example.Firstproject.controller;public class ArticleController {
+package com.example.Firstproject.controller;
+
+import com.example.Firstproject.DTO.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create") // mustache에서 action과 같
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
 }
