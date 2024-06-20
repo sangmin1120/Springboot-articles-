1b2a78e7f8ede57600b0c40c42bc91ba0b5daccf
sangmin1120 <sangmin8276@gmail.com>
2024-06-20 14:56:06 +0900

    DB에 저장할 수 있는 객체 Entity

--- src/main/java/com/example/Firstproject/entity/Article.java
+++ src/main/java/com/example/Firstproject/entity/Article.java
@@ -1,2 +1,33 @@
-package com.example.Firstproject.entity;public class Article {
+package com.example.Firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Article {
+    @Id // 엔티티의 대푯값 지정
+    @GeneratedValue // 자동 생성 기능 추가 (숫자가 자동으로 매겨짐)
+    private Long id;
+    @Column // title 필드 선언 , DB 테이블의 title열과 연결됨
+    private String title;
+    @Column // content 필드 선언 , DB 테이블의 content열과 연결됨
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
