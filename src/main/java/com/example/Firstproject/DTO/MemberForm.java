0169fad61210cc404b59b38adf1a29df4bf5887c
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 00:29:01 +0900

    Member DTO 媛앹껜

--- src/main/java/com/example/Firstproject/DTO/MemberForm.java
+++ src/main/java/com/example/Firstproject/DTO/MemberForm.java
@@ -1,2 +1,25 @@
-package com.example.Firstproject.DTO;public class MemberForm {
+package com.example.Firstproject.DTO;
+
+import com.example.Firstproject.entity.Member;
+
+public class MemberForm {
+    private String email;
+    private String password;
+
+    public MemberForm(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberForm{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Member toEntity(){
+        return new Member(null, email , password);
+    }
 }
