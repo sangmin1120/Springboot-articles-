f908f89aa31599316405076a32f46ddf85ac8556
sangmin1120 <sangmin8276@gmail.com>
2024-06-20 14:55:13 +0900

    entity를 DB에 저장하는 과정 추가

--- src/main/java/com/example/Firstproject/controller/ArticleController.java
+++ src/main/java/com/example/Firstproject/controller/ArticleController.java
@@ -1,12 +1,17 @@
 package com.example.Firstproject.controller;
 
+import com.example.Firstproject.entity.Article;
+import com.example.Firstproject.repository.ArticleRepository;
 import com.example.Firstproject.DTO.ArticleForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired // 스프링 부트가 미리 생성해 놓은 리파지토리 객체 주입(DI : dependenct injection 의존성 주입)
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm(){
@@ -17,6 +22,13 @@ public class ArticleController {
     @PostMapping("/articles/create") // mustache에서 action과 같
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
