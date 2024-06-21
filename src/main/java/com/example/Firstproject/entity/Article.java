39ec38493b14959dee3a4c150608949da28d41ae
sangmin1120 <sangmin8276@gmail.com>
2024-06-22 00:17:30 +0900

    링크와 redirect 설정

--- src/main/java/com/example/Firstproject/entity/Article.java
+++ src/main/java/com/example/Firstproject/entity/Article.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor // 기본 생성자 추가 어노테이션
 @ToString
+@Getter
 @Entity
 public class Article {
     @Id // 엔티티의 대푯값 지정
