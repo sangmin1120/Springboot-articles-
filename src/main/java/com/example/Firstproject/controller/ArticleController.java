192401477df850ed278b999a190cc0af3c83aa6e
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 23:40:20 +0900

    article 조회 메소드 추가

--- src/main/java/com/example/Firstproject/controller/ArticleController.java
+++ src/main/java/com/example/Firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.Firstproject.DTO.ArticleForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j // 로깅 기능을 위한 어노테이션 추가
@@ -32,5 +34,18 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
+    public String show(@PathVariable Long id , Model model){ // 매개변수로 id 받아 오기
+        log.info("id = " +id); // id를 잘 받았는 지 확인
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article" , articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
