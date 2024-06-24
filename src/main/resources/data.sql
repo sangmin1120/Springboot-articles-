92ce2051ddb8729ce918c94bc50a4b2155d4b1d3
sangmin1120 <sangmin8276@gmail.com>
2024-06-25 00:49:04 +0900

    create문 수정 (DB에서 PK 부여)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into article(id,title,content) values(1,'가가가가','1111');
-insert into article(id,title,content) values(2,'나나나나','2222');
-insert into article(id,title,content) values(3,'다다다다','3333');
-insert into member(id,email,password) values(1,'111@111','1234');
-insert into member(id,email,password) values(2,'222@222','qwer');
-insert into member(id,email,password) values(3,'333@333','asdf');
+insert into article(title,content) values('가가가가','1111');
+insert into article(title,content) values('나나나나','2222');
+insert into article(title,content) values('다다다다','3333');
+insert into member(email,password) values('111@111','1234');
+insert into member(email,password) values('222@222','qwer');
+insert into member(email,password) values('333@333','asdf');
 
