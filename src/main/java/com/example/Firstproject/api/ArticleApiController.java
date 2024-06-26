deb54fe4549120ce86bb392b1074bb310b3e57eb
sangmin1120 <sangmin8276@gmail.com>
2024-06-26 15:14:33 +0900

    RestAPI GET 援ы쁽

--- src/main/java/com/example/Firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/Firstproject/api/ArticleApiController.java
@@ -1,2 +1,32 @@
-package com.example.Firstproject.api;public class ArticleApiController {
+package com.example.Firstproject.api;
+
+import com.example.Firstproject.entity.Article;
+import com.example.Firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+    @Autowired // 리파지토리 주입
+    private ArticleRepository articleRepository;
+
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index(){
+        return (List<Article>)articleRepository.findAll();
+    }
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id){
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    // POST
+
+    // PATCH
+
+    // DELETE
 }
