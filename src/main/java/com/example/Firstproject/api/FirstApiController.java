4a52e1f01dc4edfdff6e7930b5a7cb6f649fc49f
sangmin1120 <sangmin8276@gmail.com>
2024-06-26 15:14:15 +0900

    RestAPI 연습

---
+++ src/main/java/com/example/Firstproject/api/FirstApiController.java
@@ -0,0 +1,13 @@
+package com.example.Firstproject.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // REST API 컨트롤러
+public class FirstApiController {
+    @GetMapping("/api/hello")
+    public  String hello(){
+        return "hello world!";
+    }
+
+}
