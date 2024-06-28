8e20a55df97240e387ac2c907bacad4076638817
sangmin1120 <sangmin8276@gmail.com>
2024-06-28 18:30:04 +0900

    SpringbootTest ing

--- src/test/java/com/example/Firstproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/Firstproject/repository/CommentRepositoryTest.java
@@ -1,4 +1,32 @@
+package com.example.Firstproject.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DataJpaTest
 class CommentRepositoryTest {
-  
+    @Autowired
+    CommentRepository commentRepository;
+    @Test
+    void findByArticleId() {
+        // 1. 예상
+        
+        // 2. 실제
+
+        // 3. 비교
+    }
+
+    @Test
+    void findByNickname() {
+        // 1. 예상
+
+        // 2. 실제
+
+        // 3. 비교
+    }
 }
