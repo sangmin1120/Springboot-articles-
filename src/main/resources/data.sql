f697184a68df63aa65216f2425928029eceb9159
sangmin1120 <sangmin8276@gmail.com>
2024-06-28 18:26:51 +0900

    초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,17 @@ insert into coffee(name,price) values('아메리카노','4500');
 insert into coffee(name,price) values('라떼','5000');
 insert into coffee(name,price) values('카페 모카','5500');
 
+insert into article(title,content) values('당신의 인생 영화는?','댓글 고');
+insert into article(title,content) values('당신의 소울 푸드는?','댓글 고고');
+insert into article(title,content) values('당신의 취미는?','댓글 고고고');
+
+insert into comment(article_id,nickname,body) values(4,'Park','굿 윌 헌팅');
+insert into comment(article_id,nickname,body) values(4,'kim','아이 엠 샘');
+insert into comment(article_id,nickname,body) values(4,'Choi','쇼생크 탈출');
+insert into comment(article_id,nickname,body) values(5,'Park','치킨');
+insert into comment(article_id,nickname,body) values(5,'kim','샤브샤브');
+insert into comment(article_id,nickname,body) values(5,'Choi','chqkq');
+insert into comment(article_id,nickname,body) values(6,'Park','조깅');
+insert into comment(article_id,nickname,body) values(6,'Kim','유튜브 시청');
+insert into comment(article_id,nickname,body) values(6,'Choi','독서');
+
