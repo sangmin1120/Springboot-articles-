83852aa5241f8b20dcf1ba2438db68d1752527d9
sangmin1120 <sangmin8276@gmail.com>
2024-06-28 18:27:51 +0900

    CommentRepository 생성 중

--- src/main/java/com/example/Firstproject/repository/CommentRepository.java
+++ src/main/java/com/example/Firstproject/repository/CommentRepository.java
@@ -3,14 +3,13 @@ package com.example.Firstproject.repository;
 import com.example.Firstproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
-public interface CommnetRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 게시글의 모든 댓글 조회
-    @Query(value="selet * from comment where article_id = :articleId" , nativeQuery = true)
-    List<Comment> finByArticleId(String articleId);
+    @Query(value="select * from comment where article_id = :articleId" , nativeQuery = true)
+    List<Comment> findByArticleId(String articleId);
 
     // 특정 닉네임의 모든 댓글 조회
     List<Comment> findByNickname(String nickname); // 이 메서드를 수행할 쿼리를 orm.xml에 작성
