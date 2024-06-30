193f1bb918ebd2a1bbc11b021b11886a50af629e
sangmin1120 <sangmin8276@gmail.com>
2024-06-30 17:25:52 +0900

    Comment CRUD 援ы쁽

--- src/main/java/com/example/Firstproject/api/CommentApiController.java
+++ src/main/java/com/example/Firstproject/api/CommentApiController.java
@@ -1,2 +1,61 @@
-package com.example.Firstproject.api;public class CommentApiController {
+package com.example.Firstproject.api;
+
+import com.example.Firstproject.DTO.CommentDto;
+import com.example.Firstproject.entity.Comment;
+import com.example.Firstproject.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentApiController {
+    @Autowired
+    private CommentService commentService;
+
+    // 댓글 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId){
+        // 서비스에 위임
+        List<CommentDto> dtos = commentService.comments(articleId);
+
+        // 결과 응답
+        return (dtos != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(dtos):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId , @RequestBody CommentDto dto){
+        CommentDto commentDto = commentService.create(articleId,dto);
+        return (commentDto != null)?
+                ResponseEntity.status(HttpStatus.OK).body(commentDto):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id , @RequestBody CommentDto dto){
+        // 서비스 위임
+        CommentDto updated = commentService.update(id,dto);
+        // 반환
+        return (updated != null)?
+                ResponseEntity.status(HttpStatus.OK).body(updated):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id){
+        // 서비스 위임
+        CommentDto deleted = commentService.delete(id);
+
+        // 반환
+        return (deleted != null)?
+                ResponseEntity.status(HttpStatus.OK).body(deleted):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
--- src/main/java/com/example/Firstproject/service/CommentService.java
+++ src/main/java/com/example/Firstproject/service/CommentService.java
@@ -1,2 +1,82 @@
-package com.example.Firstproject.service;public class CommentService {
+package com.example.Firstproject.service;
+
+import com.example.Firstproject.DTO.CommentDto;
+import com.example.Firstproject.entity.Article;
+import com.example.Firstproject.entity.Comment;
+import com.example.Firstproject.repository.ArticleRepository;
+import com.example.Firstproject.repository.CommentRepository;
+import jakarta.transaction.Transactional;
+import jdk.jfr.StackTrace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId){
+//        List<Comment> comments = commentRepository.findByArticleId(articleId);
+//        // 엔티티를 DTO로 변환
+//        List<CommentDto> dtos = new ArrayList<CommentDto>();
+//        for (int i=0;i<comments.size();i++){
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c);
+//            dtos.add(dto);
+//        }
+
+        return commentRepository.findByArticleId(articleId)
+                .stream().map(comment -> CommentDto.createCommentDto(comment)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // article 찾기
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " + " 대상 게시글이 없습니다"));
+
+        // dto->엔티티 변환
+        Comment comment = Comment.create(dto,article);
+
+        // 생성
+        Comment created = commentRepository.save(comment);
+
+        // 변환
+        return CommentDto.createCommentDto(created);
+
+    }
+
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        // 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" + "대상 댓글이 없습니다."));
+
+        // 댓글 수정
+        target.patch(dto);
+
+        // DB로 갱신
+        Comment updated = commentRepository.save(target);
+
+        // 댓글 엔티티를 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(updated);
+    }
+
+    @Transactional
+    public CommentDto delete(Long id) {
+        // 엔티티 찾아 , 예외
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 삭제 실패! 댓글을 몾 찾았습니다."));
+
+        // 삭제
+        commentRepository.delete(target);
+
+        return CommentDto.createCommentDto(target);
+    }
 }
