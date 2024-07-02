f5b85fb11009ead9f963985251b30b84d7f87658
sangmin1120 <sangmin8276@gmail.com>
2024-07-02 15:54:57 +0900

    lombok:Getter 추가

--- src/main/java/com/example/Firstproject/DTO/MemberForm.java
+++ src/main/java/com/example/Firstproject/DTO/MemberForm.java
@@ -2,10 +2,12 @@ package com.example.Firstproject.DTO;
 
 import com.example.Firstproject.entity.Member;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
+@Getter
 public class MemberForm {
     private Long id;
     private String email;
