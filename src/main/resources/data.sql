40decf65bf65b647d3596ecefa160792df7b9052
sangmin1120 <sangmin8276@gmail.com>
2024-07-21 16:36:35 +0900

    fix: ArticleDto memebrId 삭제
    article dto에서 memberId column을 삭제해서 sql문으로 생성할 때도 memberid를 삭제해 주었다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 insert into member(email,password) values('111@111','1234');
 insert into member(email,password) values('222@222','qwer');
 insert into member(email,password) values('333@333','asdf');
-insert into article(member_id,title,content) values(1,'가가가가','1111');
-insert into article(member_id,title,content) values(2,'나나나나','2222');
-insert into article(member_id,title,content) values(3,'다다다다','3333');
+insert into article(title,content) values('가가가가','1111');
+insert into article(title,content) values('나나나나','2222');
+insert into article(title,content) values('다다다다','3333');
 
 insert into coffee(name,price) values('아메리카노','4500');
 insert into coffee(name,price) values('라떼','5000');
 insert into coffee(name,price) values('카페 모카','5500');
 
-insert into article(member_id,title,content) values(1,'당신의 인생 영화는?','댓글 고');
-insert into article(member_id,title,content) values(2,'당신의 소울 푸드는?','댓글 고고');
-insert into article(member_id,title,content) values(3,'당신의 취미는?','댓글 고고고');
+insert into article(title,content) values('당신의 인생 영화는?','댓글 고');
+insert into article(title,content) values('당신의 소울 푸드는?','댓글 고고');
+insert into article(title,content) values('당신의 취미는?','댓글 고고고');
 
 insert into comment(article_id,nickname,body) values(4,'Park','굿 윌 헌팅');
 insert into comment(article_id,nickname,body) values(4,'kim','아이 엠 샘');
