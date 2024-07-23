d2368a87c5c5261ab0eef3fb8694cf9641e060b7
sangmin1120 <sangmin8276@gmail.com>
2024-07-23 13:38:20 +0900

    feat: findByEmail 추가
    이메일을 통해 entity 찾기 추가

--- src/main/java/com/example/Firstproject/repository/MemberRepository.java
+++ src/main/java/com/example/Firstproject/repository/MemberRepository.java
@@ -1,7 +1,15 @@
 package com.example.Firstproject.repository;
 
+import com.example.Firstproject.entity.Comment;
 import com.example.Firstproject.entity.Member;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends CrudRepository<Member,Long> {
+
+    // 이메일로 조회
+    @Query(value="select * from member where email = :input_email" , nativeQuery = true)
+    Member findByEamil(String input_email);
 }
