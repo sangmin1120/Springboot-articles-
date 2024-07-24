67c84ad05f344846c1cc3869869b329aded628e9
sangmin1120 <sangmin8276@gmail.com>
2024-07-24 15:25:17 +0900

    feat: memberid FK 추가 , createArticleForm 메소드 추가

--- src/main/java/com/example/Firstproject/DTO/ArticleForm.java
+++ src/main/java/com/example/Firstproject/DTO/ArticleForm.java
@@ -11,10 +11,18 @@ import lombok.ToString;
 @Getter
 public class ArticleForm {
     private Long id; // update할 때 id를 히든으로 가져와서 추가함
+    private Long memberId;
     private String title; //제목을 받을 필드
     private String content; // 내용을 받을 필드
 
-    public Article toEntity() {
-        return new Article(id,title,content);
+//    public Article toEntity() {
+//        return new Article(id,title,content);
+//    }
+
+    public static ArticleForm createArticleForm(Article a){
+        return new ArticleForm(a.getId(),
+                a.getMember().getId(),
+                a.getTitle(),
+                a.getContent());
     }
 }
